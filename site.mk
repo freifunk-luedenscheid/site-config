d959246dd1c54159e222dcd50e69e4478a8de755
AKA-47 <freifunk@servicebackup.de>
2016-09-13 18:35:35 +0200

    site at 16.09.1

---
+++ site.mk
@@ -0,0 +1,124 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-core \
+	gluon-config-mode-hostname \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-luci-node-role \
+	gluon-next-node \
+	gluon-neighbour-info \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+    simple-tc \
+	gluon-aptimeclock \
+	ffls-banner \
+	ffls-debug \
+	iwinfo \
+	iptables \
+	gluon-ebtables-filter-roguenets \
+	haveged
+
+# add offline ssid only if the target has wifi device
+ifeq ($(GLUON_TARGET),ar71xx-generic)
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer \
+        gluon-txpowerfix \
+	ffls-ath9k-broken-wifi-workaround \
+        gluon-forceradioenable \
+	gluon-weeklyreboot 
+endif
+
+ifeq ($(GLUON_TARGET),ar71xx-nand)
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer \
+        gluon-txpowerfix \
+	ffls-ath9k-broken-wifi-workaround \
+        gluon-forceradioenable \
+        gluon-weeklyreboot  
+
+endif
+
+ifeq ($(GLUON_TARGET),mpc85xx-generic)
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer \
+        gluon-txpowerfix \
+	ffls-ath9k-broken-wifi-workaround \
+        gluon-forceradioenable \
+        gluon-weeklyreboot  
+
+endif
+
+# add addition network drivers and usb stuff only to targes where disk space does not matter.
+ifeq ($(GLUON_TARGET),x86-generic)
+GLUON_SITE_PACKAGES += \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+	kmod-atl2
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+GLUON_SITE_PACKAGES += \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+	kmod-atl2
+endif
+
+
+##	DEFAULT_GLUON_RELEASE
+#		version string to use for images
+#		gluon relies on
+#			opkg compare-versions "$1" '>>' "$2"
+#		to decide if a version is newer or not.
+
+DEFAULT_GLUON_RELEASE := 16.09.1
+
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Default priority for updates.
+GLUON_PRIORITY ?= 0
+
+# Languages to include
+GLUON_LANGS ?= de
+
+# if set autoupdater is active while make GLUON_BRANCH=stable
+GLUON_BRANCH ?= stable
+export GLUON_BRANCH
+
