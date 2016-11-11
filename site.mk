8fcc1b9748a7d174a272fbb40cbedeadd81eaf15
AKA-47 <freifunk@servicebackup.de>
2016-11-11 21:41:31 +0100

    Added new Site for 2016.1.2

--- site.mk
+++ site.mk
@@ -97,7 +97,7 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 16.09.5i
+DEFAULT_GLUON_RELEASE := 16.11.1
 
 
 ##	GLUON_RELEASE
